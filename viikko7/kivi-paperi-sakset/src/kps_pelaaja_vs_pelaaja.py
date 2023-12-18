44d99849e6a0e7f1b6077c9608d37cf943cb5c39
arttu <arttu.salminen@helsinki.fi>
2023-12-18 14:01:22 +0200

    viikko7, t3ja4

---
+++ viikko7/kivi-paperi-sakset/src/kps_pelaaja_vs_pelaaja.py
@@ -0,0 +1,9 @@
+from kps import KiviPaperiSakset
+
+# luokka perii luokan KiviPaperiSakset
+class KPSPelaajaVsPelaaja(KiviPaperiSakset):
+    # toteutetaan metodi pelityypin mukaisesti
+    def _toisen_siirto(self, ensimmaisen_siirto):
+        tokan_siirto = input("Toisen pelaajan siirto: ")
+
+        return tokan_siirto
---
+++ viikko7/kivi-paperi-sakset/src/kps_tekoaly.py
@@ -0,0 +1,13 @@
+from kps import KiviPaperiSakset
+from tekoaly import Tekoaly
+
+class KPSTekoaly(KiviPaperiSakset):
+    def __init__(self):
+        self.tekoaly = Tekoaly()
+
+    def _toisen_siirto(self, ensimmaisen_siirto):
+        tokan_siirto = self.tekoaly.anna_siirto()
+        self.tekoaly.aseta_siirto(ensimmaisen_siirto)
+
+        return tokan_siirto
+
